5006f5c70cf11a118d5099c3e22fae4d2072d45f
jahoefne <jahoefne@htwg-konstanz.de>
2014-09-23 13:11:28 +0200

    connect to MongoDB and store game objects

--- app/model/GameDatabase.scala
+++ app/model/GameDatabase.scala
@@ -1,10 +1,29 @@
 package model
 
+import com.mongodb.{MongoException, BasicDBObject}
+import com.mongodb.casbah.MongoClient
+import com.mongodb.casbah.commons.MongoDBObject
+
 /**
  * User: Jan Moritz Hoefner
  * Date: 23/09/14
  * Time: 12:47
  */
-object GameDatabase {
 
+case class Game(uuid: String)
+
+object GameDatabase {
+  val mongoClient =  MongoClient("127.0.0.1", 27017)
+
+  val db = mongoClient("Chess-Online")
+  val collection = db("Games")
+
+  def addGame(game: Game): Game ={
+      try {
+        collection.insert(game)
+        game
+      }catch{
+        case ex: Exception => null
+      }
+  }
 }
