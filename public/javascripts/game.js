098ab42d6aa15b1fc09fa5131414a9bced58a1cd
jahoefne <jahoefne@htwg-konstanz.de>
2014-09-27 16:17:35 +0200

    set UUID as global js-variable, construct Websockets URI

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,5 +1,13 @@
 var game = {
+
+    webSocketUri : "ws://" + window.location.host + "/socket/" + window._global_uuid,
+
     clickedField: function(x,y){
         console.log(x+","+y);
     }
+
+    /**
+     *  Connect to Server using Websockets
+     */
+
 }
