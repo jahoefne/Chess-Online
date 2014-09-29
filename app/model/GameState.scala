f007a37b904f656a4bad25ce3d82f2617996195f
jahoefne <jahoefne@htwg-konstanz.de>
2014-09-29 14:28:56 +0200

    implicit writes for Json conversion

--- app/model/GameState.scala
+++ app/model/GameState.scala
@@ -1,18 +1,20 @@
 package model
 
 import controller.GameController
-
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 /**
  * Represents the state of a chess game, can be stored in Database.
  * Can be used to create GameControllers.
  */
 case class GameState(uuid: String,
-                     field: Array[Array[Byte]],
+                     field: Array[Array[Int]],
                      check: Boolean,
                      whiteOrBlack: Byte,
                      gameOver: Boolean)
 
 object GameState{
+
 
   /**
    * Extract game state out of a GameController
@@ -31,4 +33,16 @@ object GameState{
     state.gameOver,
     state.check,
     new Field(state.field, state.whiteOrBlack))
+
+  implicit val gameStateWrites = new Writes[GameState] {
+    def writes(c: GameState): JsValue = {
+      Json.obj(
+        "uuid" -> c.uuid,
+        "field" -> c.field,
+        "check" -> c.check,
+        "whiteOrBlack" -> c.whiteOrBlack.toInt,
+        "gameOver" -> c.gameOver
+      )
+    }
+  }
 }
