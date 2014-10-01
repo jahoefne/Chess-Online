1d7be13d726e3a8926d72389dd504fd0c386a80d
jahoefne <jahoefne@htwg-konstanz.de>
2014-10-01 16:37:14 +0200

    Don't store model in  DB for now, wait until Model Classes are fixed

--- app/model/GameDB.scala
+++ app/model/GameDB.scala
@@ -1,48 +1,47 @@
 package model
+/*
+Database currently not enabled
 
 import com.mongodb.casbah.MongoClient
 import com.mongodb.casbah.commons.MongoDBObject
-import com.novus.salat._
 import controller.GameController
 import play.api.Logger
 import play.api.Play.current
 
-import scala.collection.mutable
+import scala.collection.mutable   */
 
-/**
- * Contains a Cache and a Connection to the MongoDB
- * Responsible for persisting GameState and keeping the
- * database-load low
- */
-object DB {
-    val mongoClient = MongoClient("127.0.0.1", 27017)
-    val db = mongoClient("Chess-Online")
-    val collection = db("Games")
-
-    /**
-     * Loads a GameState, based on a uuid from the Database
-     */
-    def loadGame(uuid: String): GameController = {
-      val dbObj = new MongoDBObject(collection.findOne(MongoDBObject("uuid" -> uuid)).get)
-      val controller = GameState.toGameController(dbObj)
-      Cache addToCache(uuid, controller)
-      controller
-    }
-
-    /**
-     * Saves a game state in the Database, updates if the game existed already,
-     * inserts if it did not exist.
-     */
-    def saveGame(uuid: String): Object = {
-      try {
-        val query = MongoDBObject("uuid" -> uuid)
-        val state = GameState.fromGameController(Cache.getController(uuid), uuid)
-        collection.update(query, state, upsert = true)
-      } catch {
-        case ex: Exception =>
-          log.error(ex.toString)
-          ex
-      }
-    }
-  }
-}
+///**
+// * Connection to the MongoDB stores GameStateObject
+// **/
+//object GameDB {
+//    val mongoClient = MongoClient("127.0.0.1", 27017)
+//    val db = mongoClient("Chess-Online")
+//    val collection = db("Games")
+//
+//    val log = Logger(this getClass() getName())
+//
+//    /**
+//     * Loads a GameState, based on a uuid from the Database
+//     */
+//    def loadGame(uuid: String): GameState = {
+//      val dbObj = new MongoDBObject(collection.findOne(MongoDBObject("uuid" -> uuid)).get)
+//      val controller = GameState.toGameController(dbObj)
+//      controller
+//    }
+//
+//    /**
+//     * Saves a game state in the Database, updates if the game existed already,
+//     * inserts if it did not exist.
+//     */
+//    def saveGame(uuid: String, state: GameState): Object = {
+//      try {
+//        val query = MongoDBObject("uuid" -> uuid)
+//        collection.update(query, state, upsert = true)
+//      } catch {
+//        case ex: Exception =>
+//          log.error(ex.toString)
+//          ex
+//      }
+//    }
+//  }
+//}
