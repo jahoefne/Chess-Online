0cdfadac50b460b6487223059f373e9ba9efb8de
jahoefne <jahoefne@htwg-konstanz.de>
2014-10-01 20:42:21 +0200

    added status bar

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -18,9 +18,9 @@ var game = {
     onOpen: function(){
         console.log(this.socket);
         var message = { type: "GetGame" }
+        this.sendMessage(message);
         var message = { type: "GetRole" }
-
-        this.send(message);
+        this.sendMessage(message);
     },
 
     onClose: function(event){
@@ -36,16 +36,27 @@ var game = {
        var msg = JSON.parse(event.data);
        switch(msg.type){
         case "Role":
-            alert(msg.role);
+            $("#status").text("You're the "+msg.role);
             break;
+
+        case "ActiveGame":
+            this.updateField(msg);
+            break;
+
         default:
             break;
        };
     },
 
-    send: function(msg){
-        this.socket.send(JSON.stringify(msg));
-    },
+	sendMessage : function(message) {
+		console.log("sendMessage");
+		if (this.socket.readyState !== this.socket.OPEN) {
+			console.log("Error sending message")
+			return false;
+		}
+		this.socket.send(JSON.stringify(message));
+		return true;
+	},
 
     updateField: function(msg){
         $( ".field" ).each(function(i) {
