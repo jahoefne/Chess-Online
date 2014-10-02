5b0d3b1ccf7714984b7b96dbb5c123ac60784bce
jahoefne <jahoefne@htwg-konstanz.de>
2014-10-02 13:40:06 +0200

    postStop method + comments

--- app/controllers/ChessWebSocketActor.scala
+++ app/controllers/ChessWebSocketActor.scala
@@ -3,7 +3,7 @@ package controllers
 import java.util.UUID
 
 import akka.actor.{Actor, Props, ActorRef}
-import model.{ActiveGame, ActiveGameStore}
+import model.ActiveGameStore
 import play.api.libs.json._
 
 /**
@@ -15,31 +15,34 @@ class ChessWebSocketActor(out: ActorRef,
 
   def receive = {
     case msg: JsValue => (msg \ "type").as[String] match {
-      /**
-       * Return the current game state
-       */
 
+      /** Return current game state */
       case "GetGame" => out ! ActiveGameStore.getActiveGame(gameID).toJson
 
+      /** Return the role of the player: white, black, spectator */
       case "GetRole" => out ! Json.obj(
         "type" -> "Role",
         "role" -> ActiveGameStore.getActiveGame(gameID).getRole(playerID))
 
-
-      /**
-       * Move figure at src to tgt
-       */
+      /** Move figure at src to tgt */
       case "Move" => val uuid = (msg \ "uuid").as[String]
 
-      /**
-       * Get possible moves for a field x
-       */
+      /** Get possible moves for a field x */
       case "PossibleMoves" => println("PossibleMoves")
 
       case _ => out ! "Unknown Json"
     }
 
     case _ => out ! "No Message Type supplied!"
+  }
+
+
+  /**
+   * Socket was closed from the client
+   */
+  override def postStop() = {
+     println("Websocket closed from client")
+    // TODO: remove player from activeGame
   }
 }
 
