87eaaaa81f5b4025b601a5b71e337bcaabab83f5
jahoefne <jahoefne@htwg-konstanz.de>
2014-10-02 13:43:40 +0200

    comments

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,38 +8,29 @@ import play.api.Play.current
 
 object Application extends Controller {
 
-  /**
-   * Landing Page
-   */
+  /** Landing Page */
   def index = Action {
     Ok(views.html.index())
   }
 
-  /**
-   * Create a new game instance
-   */
+  /** Create a new game instance */
   def newGame = Action {
     val gameUUID = java.util.UUID.randomUUID.toString
     ActiveGameStore.add(gameUUID, ActiveGame(gameUUID, new GameController(), null, null, List()))
     Redirect(routes.Application.game(gameUUID))
   }
 
-  /**
-   * Access existing game instance
-   */
+  /** Access existing game instance */
   def game(uuid: String) = Action {
     Ok(views.html.game(uuid))
   }
 
-  /**
-   * Create websocket for game: uuid
-   */
+  /** Create websocket for game: uuid */
   def socket (uuid: String) = WebSocket.acceptWithActor[JsValue, JsValue] {
-    request =>
-      out =>
-        val playerID = java.util.UUID.randomUUID
-        val activeGame =  ActiveGameStore.getActiveGame(uuid).addPlayer(new Player(playerID, out))
-        ActiveGameStore.add(uuid, activeGame)
-        ChessWebSocketActor.props(out, playerID , uuid)
+    request => out =>
+      val playerID = java.util.UUID.randomUUID
+      val activeGame =  ActiveGameStore.getActiveGame(uuid).addPlayer(new Player(playerID, out))
+      ActiveGameStore.add(uuid, activeGame)
+      ChessWebSocketActor.props(out, playerID , uuid)
   }
 }
