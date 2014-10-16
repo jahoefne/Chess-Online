23be7cad9b3696c0c167b732e961adb70a4c6ed8
jahoefne <jahoefne@htwg-konstanz.de>
2014-10-16 15:07:53 +0200

    separate game.js in board.js and websocket.js

---
+++ public/javascripts/board.js
@@ -0,0 +1,72 @@
+var board = {
+    playerID: window._global_playerID,
+
+    myTurn: false,
+
+    lastClicked:null,
+
+    clickedField: function(x,y){
+        if(this.myTurn){
+             if(this.lastClicked != null){
+                 this.unhighlightAll();
+                 this.move(this.lastClicked.x, this.lastClicked.y, x, y);
+                 this.lastClicked = null;
+             }else{
+                 this.lastClicked = {el:$("#"+x+""+y), x:x, y:y};
+                 this.highlight(this.lastClicked.el);
+
+                 // query possible moves
+                 var message = {type: "PossibleMoves", x: x, y: y};
+
+                 websocket.sendMessage(message);
+             }
+         }
+    },
+
+    highlight: function(el){
+        el.addClass("highlight");
+    },
+
+    unhighlightAll: function(){
+         $( ".field" ).each(function() {
+                $( this ).removeClass("highlight");
+         });
+    },
+
+    move: function(srcX, srcY, dstX, dstY){
+        console.log($("#"+srcX+""+srcY).offset());
+        var msg = {type:"Move", srcX: srcX, srcY: srcY, dstX:dstX, dstY:dstY}
+        websocket.sendMessage(msg);
+    },
+
+    gotActiveGameMsg: function(msg){
+            this.updateField(msg);
+            if(msg.white == "" || msg.black == ""){
+               $("#header-bar").text("Waiting for other player!");
+               return;
+            }
+
+            if(this.playerID == msg.white && msg.whiteOrBlack > 0  // we are white player and it's our turn
+               || this.playerID == msg.black && msg.whiteOrBlack < 0 ){  // we are black player and it's our turn
+                 this.myTurn = true;
+                 $("#header-bar").text("It's your turn!");
+            }else{
+                 this.yourTurn = false;
+                  $("#header-bar").text("Please wait.");
+            }
+
+            if(msg.gameOver){
+                $("#header-bar").text("Game over");
+                 this.yourTurn = false;
+            }
+    },
+
+    updateField: function(msg){
+        console.log("Update field");
+        $( ".field" ).each(function(i) {
+            var x = Math.floor(i/8);
+            var y = i%8;
+            $( this ).text(figures[msg.field[x][y]]);
+        });
+    }
+};
---
+++ public/javascripts/websocket.js
@@ -0,0 +1,56 @@
+var websocket = {
+    socket: null,
+    uri: "ws://" + window.location.host + "/socket/" + window._global_uuid + "/"+ window._global_playerID,
+    playerID: window._global_playerID,
+
+    init: function() {
+        this.socket = new WebSocket(this.uri);
+        this.socket.onopen = this.onOpen.bind(this);
+        this.socket.onmessage = this.onMessage.bind(this);
+    },
+
+    onOpen: function(){
+        var message = { type: "GetGame" }
+        this.sendMessage(message);
+        var message = { type: "GetRole" }
+        this.sendMessage(message);
+    },
+
+    onMessage: function(event){
+       var msg = JSON.parse(event.data);
+       switch(msg.type){
+
+        case "Role":
+            $("#status").text("You're the "+msg.role);
+            break;
+
+        case "ActiveGame":
+            board.gotActiveGameMsg(msg);
+            break;
+
+        case "PossibleMoves":
+            for(var i=0; i < msg.moves.length; i++){
+               $("#"+msg.moves[i][0]+""+msg.moves[i][1]).addClass("highlight");
+            }
+            break;
+
+        default:
+            break;
+       };
+    },
+
+	sendMessage : function(message) {
+		console.log("sendMessage");
+		if (this.socket.readyState !== this.socket.OPEN) {
+			console.log("Error sending message")
+			return false;
+		}
+		this.socket.send(JSON.stringify(message));
+		return true;
+	}
+};
+
+// Init the Game-JS after the document is ready
+$(document).ready(function(){
+    websocket.init();
+});
